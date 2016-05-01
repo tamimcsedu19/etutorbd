ab9e8f19823a9ecda4f4ecdba1c7aa4500dfd72a
Tamim Addari <tamim.tamim1382@gmail.com>
2016-05-01 10:36:44 +0600

    partials ok

---
+++ public/js/dataServices.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/* Services */
+
+(function() {
+
+  angular
+      .module('Etutor')
+      .service('meanData', meanData);
+
+  meanData.$inject = ['$http', 'authentication'];
+  function meanData ($http, authentication) {
+
+    var getProfile = function () {
+      return $http.get('/api/profile', {
+        headers: {
+          Authorization: 'Bearer '+ authentication.getToken()
+        }
+      });
+    };
+
+    return {
+      getProfile : getProfile
+    };
+  }
+
+})();
