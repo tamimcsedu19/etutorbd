612bd43a26c4a4c0e7cceeffbc0b79ffcc472f7d
Tamim Addari <tamim.tamim1382@gmail.com>
2016-05-09 17:35:00 +0600

    Updated the routes for serving index.html on certain pages.

---
+++ routes/GenericRoutes.js
@@ -0,0 +1,21 @@
+/**
+ * Created by tamim on 5/9/16.
+ */
+'use-strict'
+var express = require('express');
+var router = express.Router();
+router.get('/login',function(req,res){
+
+    res.sendFile(global.__base+'/public/index.html', function (err) {
+        console.log(err);
+    });
+});
+router.get('/register',function(req,res){
+
+    res.sendFile(global.__base+'/public/index.html', function (err) {
+        console.log(err);
+    });
+});
+
+
+module.exports = router;
--- routes/RouteManager.js
+++ routes/RouteManager.js
@@ -5,8 +5,13 @@
 var searchRoutes = require('../SearchService/SearchRoutes');
 var registrationRoutes = require('../RegistrationService/RegistrationRoutes');
 var loginRoutes = require('../AuthenticationService/LoginRoutes');
+var genericRoutes = require('./GenericRoutes');
+
+
+
 exports.addRoutes = function (app){
 
+    app.use('/',genericRoutes);
     app.use('/api',searchRoutes);
     app.use('/api',registrationRoutes);
     app.use('/api',loginRoutes);
