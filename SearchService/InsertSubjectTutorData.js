185da61d0b8ba2770959f1a4090fe36776f3602a
Tamim Addari <tamim.tamim1382@gmail.com>
2016-05-13 20:46:52 +0600

    Added subject tutor data script

---
+++ SearchService/InsertSubjectTutorData.js
@@ -0,0 +1,11 @@
+/**
+ * Created by tamim on 5/13/16.
+ */
+var subjectToTutorDA = require('./SubjectToTutorDA');
+subjectToTutorDA.saveSubjectTutor({
+    subject:'C++',
+
+    tutorEmail: 'tamim.tamim1382@gmail.com'}
+, function (err) {
+        
+    });
